ffcb5dc4ff759fac35064fc8b4f22cbf4b36185e
momocatracho2-gif <momocatracho2@gmail.com>
2026-05-22 14:57:59 -0400

    Update cotizador_bupa_pro.py

--- cotizador_bupa_pro.py
+++ cotizador_bupa_pro.py
@@ -1171,6 +1171,19 @@ _Cotización tarifario Bupa Seguros mayo 2026. UF ${val_uf:,}. El riesgo es cubi
 
         st.code(msg, language=None)
 
+import urllib.parse
+
+mensaje_url = urllib.parse.quote(msg)
+
+telefono_limpio = telefono.replace("+", "").replace(" ", "")
+
+wa_link = f"https://wa.me/{telefono_limpio}?text={mensaje_url}"
+
+st.link_button(
+    "📲 Abrir WhatsApp",
+    wa_link
+)
+
         import os
 
 st.markdown("## 📄 PDFs Oficiales")
